101d2497b426dd78c6312b2d48debcaee2a603e3
kelvinsclass <kelvinsclassdm@gmail.com>
2026-02-17 20:04:52 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Script basique pour des alertes ou animations futures
+console.log("Site TOOSUREBET chargé !");
+
+// Si tu veux ajouter plus, par exemple une animation sur le bouton
+document.querySelector('button').addEventListener('click', () => {
+    console.log('Bouton cliqué');
+});
